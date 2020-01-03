29cbb8c89c5174df3dfae644c2e524b5d102571c
yuriygumeniuk <yuriygumeniuk@gmail.com>
2020-01-03 23:05:36 +0200

    js best-seller slider

--- js/custom.js
+++ js/custom.js
@@ -8,12 +8,52 @@ function ibg(){
 }
 ibg();
 
+$(function() {
+	$('.header-slider').slick({
+	 		arrows: false,
+	 		dots: true,
+ 	});
 
- $(function(){
- 	$('.header-slider').slick({
+ 	$('.seller-slider').slick({
+ 		dots: false,
+ 		slidesToShow: 5, 
+ 		slidesToScroll: 1,
  		arrows: false,
- 		dots: true
- 	})
- });
+ 		infinite: false,
+ 	});
+ 	
+ 	$('#arrow-next').on('click', function() {
+  		$('.seller-slider').slick('slickNext');
+	});
 
+ 	$('#arrow-prev').on('click', function() {
+  		$('.seller-slider').slick('slickPrev');
+	});
+
+ 	$('.seller-slider').on('afterChange', function(event, slick, CurrentSlide, nextSlide, slideCount) {
+        // Делаем кнопку следующий неактивной
+        if (slick.slickCurrentSlide() + 5 === slick.slideCount) {
+			$('#arrow-next').removeClass('not-end');
+        	$('#arrow-next').addClass('end');
+        	$('#arrow-next').css('cursor','default');
+        }
+        else {
+			$('#arrow-next').removeClass('end');
+        	$('#arrow-next').addClass('not-end');
+        	$('#arrow-next').css('cursor','pointer');
+        }
+        // Делаем копку предыдущий неактивной
+        if (slick.slickCurrentSlide() !== 0 ) {
+			$('#arrow-prev').removeClass('end');
+        	$('#arrow-prev').addClass('not-end');
+        	$('#arrow-prev').css('cursor','pointer');
+        }
+        else {
+			$('#arrow-prev').removeClass('not-end');
+        	$('#arrow-prev').addClass('end');
+        	$('#arrow-prev').css('cursor','default');
+        }
+    });
+
+ });
 
