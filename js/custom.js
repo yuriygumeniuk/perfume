79c38dd71936493f7cd5a14c41892b1ab0e7b619
yuriygumeniuk <yuriygumeniuk@gmail.com>
2020-01-04 00:18:39 +0200

    slider women + men

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
 function ibg(){
 	$.each($('.ibg'), function(index, val) {
-		if($(this).find('img').length>0){
+		if($(this).find('.img_bg').length>0){
 			$(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
-			$(this).find('img').css('display','none');
+			$(this).find('.img_bg').css('display','none');
 		}
 	});
 }
@@ -12,9 +12,13 @@ $(function() {
 	$('.header-slider').slick({
 	 		arrows: false,
 	 		dots: true,
+	 		infinite: true,
+	  		autoplay: true,
+		  	autoplaySpeed: 4000,
+		  	speed: 3000,
  	});
-
- 	$('.seller-slider').slick({
+// //////////////// .best-seller-slider  //////////////////////////
+ 	$('.best-seller-slider').slick({
  		dots: false,
  		slidesToShow: 5, 
  		slidesToScroll: 1,
@@ -22,38 +26,123 @@ $(function() {
  		infinite: false,
  	});
  	
- 	$('#arrow-next').on('click', function() {
-  		$('.seller-slider').slick('slickNext');
+ 	$('#best-arrow-next').on('click', function() {
+  		$('.best-seller-slider').slick('slickNext');
 	});
 
- 	$('#arrow-prev').on('click', function() {
-  		$('.seller-slider').slick('slickPrev');
+ 	$('#best-arrow-prev').on('click', function() {
+  		$('.best-seller-slider').slick('slickPrev');
 	});
 
- 	$('.seller-slider').on('afterChange', function(event, slick, CurrentSlide, nextSlide, slideCount) {
+ 	$('.best-seller-slider').on('afterChange', function(event, slick, CurrentSlide, nextSlide, slideCount) {
         // Делаем кнопку следующий неактивной
         if (slick.slickCurrentSlide() + 5 === slick.slideCount) {
-			$('#arrow-next').removeClass('not-end');
-        	$('#arrow-next').addClass('end');
-        	$('#arrow-next').css('cursor','default');
+			$('#best-arrow-next').removeClass('not-end');
+        	$('#best-arrow-next').addClass('end');
+        	$('#best-arrow-next').css('cursor','default');
         }
         else {
-			$('#arrow-next').removeClass('end');
-        	$('#arrow-next').addClass('not-end');
-        	$('#arrow-next').css('cursor','pointer');
+			$('#best-arrow-next').removeClass('end');
+        	$('#best-arrow-next').addClass('not-end');
+        	$('#best-arrow-next').css('cursor','pointer');
         }
         // Делаем копку предыдущий неактивной
         if (slick.slickCurrentSlide() !== 0 ) {
-			$('#arrow-prev').removeClass('end');
-        	$('#arrow-prev').addClass('not-end');
-        	$('#arrow-prev').css('cursor','pointer');
+			$('#best-arrow-prev').removeClass('end');
+        	$('#best-arrow-prev').addClass('not-end');
+        	$('#best-arrow-prev').css('cursor','pointer');
         }
         else {
-			$('#arrow-prev').removeClass('not-end');
-        	$('#arrow-prev').addClass('end');
-        	$('#arrow-prev').css('cursor','default');
+			$('#best-arrow-prev').removeClass('not-end');
+        	$('#best-arrow-prev').addClass('end');
+        	$('#best-arrow-prev').css('cursor','default');
         }
     });
+// ///////////////////////////////////////////////////////////
+// ///////////////  women-seller-slider  /////////////////////
+ 	$('.women-seller-slider').slick({
+ 		dots: false,
+ 		slidesToShow: 5, 
+ 		slidesToScroll: 1,
+ 		arrows: false,
+ 		infinite: false,
+ 	});
+ 	
+ 	$('#women-arrow-next').on('click', function() {
+  		$('.women-seller-slider').slick('slickNext');
+	});
+
+ 	$('#women-arrow-prev').on('click', function() {
+  		$('.women-seller-slider').slick('slickPrev');
+	});
+
+ 	$('.women-seller-slider').on('afterChange', function(event, slick, CurrentSlide, nextSlide, slideCount) {
+        // Делаем кнопку следующий неактивной
+        if (slick.slickCurrentSlide() + 5 === slick.slideCount) {
+			$('#women-arrow-next').removeClass('not-end');
+        	$('#women-arrow-next').addClass('end');
+        	$('#women-arrow-next').css('cursor','default');
+        }
+        else {
+			$('#women-arrow-next').removeClass('end');
+        	$('#women-arrow-next').addClass('not-end');
+        	$('#women-arrow-next').css('cursor','pointer');
+        }
+        // Делаем копку предыдущий неактивной
+        if (slick.slickCurrentSlide() !== 0 ) {
+			$('#women-arrow-prev').removeClass('end');
+        	$('#women-arrow-prev').addClass('not-end');
+        	$('#women-arrow-prev').css('cursor','pointer');
+        }
+        else {
+			$('#women-arrow-prev').removeClass('not-end');
+        	$('#women-arrow-prev').addClass('end');
+        	$('#women-arrow-prev').css('cursor','default');
+        }
+    });
+//////////////////////////////////////////////////////////////
+// ///////////////  men-seller-slider  /////////////////////
+ 	$('.men-seller-slider').slick({
+ 		dots: false,
+ 		slidesToShow: 5, 
+ 		slidesToScroll: 1,
+ 		arrows: false,
+ 		infinite: false,
+ 	});
+ 	
+ 	$('#men-arrow-next').on('click', function() {
+  		$('.men-seller-slider').slick('slickNext');
+	});
+
+ 	$('#men-arrow-prev').on('click', function() {
+  		$('.men-seller-slider').slick('slickPrev');
+	});
+
+ 	$('.men-seller-slider').on('afterChange', function(event, slick, CurrentSlide, nextSlide, slideCount) {
+        // Делаем кнопку следующий неактивной
+        if (slick.slickCurrentSlide() + 5 === slick.slideCount) {
+			$('#men-arrow-next').removeClass('not-end');
+        	$('#men-arrow-next').addClass('end');
+        	$('#men-arrow-next').css('cursor','default');
+        }
+        else {
+			$('#men-arrow-next').removeClass('end');
+        	$('#men-arrow-next').addClass('not-end');
+        	$('#men-arrow-next').css('cursor','pointer');
+        }
+        // Делаем копку предыдущий неактивной
+        if (slick.slickCurrentSlide() !== 0 ) {
+			$('#men-arrow-prev').removeClass('end');
+        	$('#men-arrow-prev').addClass('not-end');
+        	$('#men-arrow-prev').css('cursor','pointer');
+        }
+        else {
+			$('#men-arrow-prev').removeClass('not-end');
+        	$('#men-arrow-prev').addClass('end');
+        	$('#men-arrow-prev').css('cursor','default');
+        }
+    });
+//////////////////////////////////////////////////////////////
 
  });
 
